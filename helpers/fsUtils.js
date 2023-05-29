871cd97c7626511d145c6b838f81b014b24f70b1
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-05-29 12:13:02 -0700

    new commit

---
+++ helpers/fsUtils.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const util = require("util");
+
+const readFromFile = util.promisify(fs.readFile);
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const readAndDelete = (deleteId, file) => {
+  fs.readFile(file, "utf-8", (err, data) => {
+    if (err) console.error(err);
+    else parsedData = JSON.parse(data);
+    for (let i = 0; i < parsedData.length; i++) {
+      const { id } = parsedData[i];
+      if (id === deleteId) parsedData.splice(i, 1);
+    }
+    writeToFile(file, parsedData);
+  });
+};
+module.exports = { readFromFile, readAndDelete, readAndAppend };
---
+++ routes/notes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const notes = express.Router();
+
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  readAndDelete,
+} = require("../helpers/fsUtils");
+
+notes.get("/", async (req, res) => {
+  const noteData = await readFromFile("./db/db.json");
+  res.json(JSON.parse(noteData));
+});
+
+notes.post("/", async (req, res) => {
+  try {
+    const newNote = await readAndAppend(
+      {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+      },
+      "./db/db.json"
+    );
+    res.status(200).json(newNote);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+notes.delete("/:id", async (req, res) => {
+  try {
+    const deleteData = await readAndDelete(req.params.id, ".db/db.json");
+    res.json({ message: `${deleteData} 'deleted'` });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = notes;
